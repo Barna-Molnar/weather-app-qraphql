b0a93b7e0da37ff113f04aa60c06c3b7a8400b5a
Barnabas Molnar <hanta911@gmail.com>
2021-08-23 12:58:11 +0200

    developing Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLazyQuery } from '@apollo/client';
+import { GET_WEATHER_QUERY } from '../graphql/Queries';
 
 function Home() {
+  const [city, setCity] = useState('');
+  const [getWeather, { loading, data, error }] = useLazyQuery(
+    GET_WEATHER_QUERY,
+    {
+      variables: { name: city },
+    }
+  );
+
+  if (error) return <h1>Error found</h1>;
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div className="home">
       <h1>Search for the weather</h1>
-      <input type="text" placeholder="city name" />
-      <button>search</button>
+      <input
+        type="text"
+        placeholder="city name"
+        onChange={(event) => {
+          setCity(event.target.value);
+        }}
+      />
+      <button onClick={() => getWeather()}>search</button>
+      {data && !loading && (
+        <>
+          <div>
+            <h1>{data.getCityByName.name}</h1>
+            <h1>{data.getCityByName.weather.temperature.actual}</h1>
+            <h1>{data.getCityByName.weather.summary.description}</h1>
+          </div>
+        </>
+      )}
     </div>
   );
 }
