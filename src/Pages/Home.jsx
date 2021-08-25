1aedc9db36cf0408d83c3aacf42e7b354421282c
Barnabas Molnar <hanta911@gmail.com>
2021-08-25 09:32:46 +0200

    Clearing inputfield with useRef

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useLazyQuery } from '@apollo/client';
 import { GET_WEATHER_QUERY } from '../graphql/Queries';
 
 function Home() {
+  const inputRef = useRef();
   const [city, setCity] = useState('');
   const [getWeather, { loading, data, error }] = useLazyQuery(
     GET_WEATHER_QUERY,
@@ -20,13 +21,22 @@ function Home() {
     <div className="home">
       <h1>Search for the weather</h1>
       <input
+        ref={inputRef}
         type="text"
         placeholder="city name"
         onChange={(event) => {
           setCity(event.target.value);
         }}
       />
-      <button onClick={() => getWeather()}>search</button>
+      <button
+        onClick={() => {
+          getWeather();
+          inputRef.current.value = '';
+          //   console.log(inputRef);
+        }}
+      >
+        search
+      </button>
       {data && !loading && (
         <>
           <div>
